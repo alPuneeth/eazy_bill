9fa90ea4ad2ae378ea5f806651366f65f6e569f1
Puneeth <puneeth.fcv@gmail.com>
2025-12-15 13:18:23 +0530

    Add customer Pydantic schemas with FK support and senitive data handling

---
+++ app/schemas/customer.py
@@ -0,0 +1,90 @@
+from pydantic import BaseModel, Field, StringConstraints
+from typing import Optional, Annotated
+from datetime import datetime
+
+NameStr = Annotated[
+    str,
+    StringConstraints(
+        strip_whitespace=True,
+        min_length=1,
+        max_length=30
+    )
+]
+
+PhoneStr = Annotated[
+    str,
+    StringConstraints(
+        strip_whitespace=True,
+        min_length=8,
+        max_length=15
+    )
+]
+
+AadhaarStr = Annotated[
+    str,
+    StringConstraints(
+        strip_whitespace=True,
+        min_length=12,
+        max_length=12,
+        pattern="^[2-9]\\d{11}$"
+    )
+]
+
+
+class CustomerCreate(BaseModel):
+    name: NameStr = Field(
+        title="Name",
+        description="Displays customer name")
+    phone: PhoneStr = Field(
+        title="Phone number",
+        description="Displays customer's phone number")
+    alternate_number: Optional[PhoneStr] = Field(default=None)
+    aadhaar_number: Optional[AadhaarStr] = Field(
+        default=None,
+        title="Aadhaar number",
+        description="Displays aadhaar number"
+    )
+    upi_id: Optional[str] = Field(
+        default=None,
+        title="UPI ID",
+        description="Displays upi id of the customer")
+
+    village_id: int = Field(
+        title="Village",
+        description="Village the customer belongs to"
+    )
+    customer_type_id: int = Field(
+        title="Customer Type",
+        description="Classification of the customer"
+    )
+    description: Optional[str] = Field(default=None)
+
+
+class CustomerRead(BaseModel):
+    model_config = {"from_attributes": True}
+
+    public_id: str
+
+    name: str
+    phone: str
+    alternate_number: Optional[str]
+    upi_id: Optional[str]
+
+    village_id: int
+    customer_type_id: int
+
+    created_at: datetime
+    updated_at: datetime
+    description: Optional[str]
+
+
+class CustomerUpdate(BaseModel):
+    name: Optional[NameStr] = Field(default=None)
+    phone: Optional[PhoneStr] = Field(default=None)
+    alternate_number: Optional[PhoneStr] = Field(default=None)
+    aadhaar_number: Optional[AadhaarStr] = Field(default=None)
+    upi_id: Optional[str] = Field(default=None)
+
+    village_id: Optional[int] = Field(default=None)
+    customer_type_id: Optional[int] = Field(default=None)
+    description: Optional[str] = Field(default=None)
