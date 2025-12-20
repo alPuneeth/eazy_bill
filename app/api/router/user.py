693815f49c01e42a4b3e4b7ea8f7c45911d93c28
Puneeth <puneeth.fcv@gmail.com>
2025-12-20 14:20:23 +0530

    feat: add User CRUD (bootstrap phase)

---
+++ app/api/router/user.py
@@ -0,0 +1,56 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import Session, select
+from sqlalchemy.exc import IntegrityError
+
+
+from app.db.session import get_session
+from app.models.core_models.user import User
+from app.schemas.user import (
+    UserCreate,
+    UserRead
+)
+
+router = APIRouter(
+    prefix="/user",
+    tags=["User"]
+    )
+
+
+@router.get("/", response_model=list[UserRead])
+def list_users(
+    session: Session = Depends(get_session)
+):
+    users = session.exec(select(User)).all()
+    return users
+
+
+@router.get("/{user_id}", response_model=UserRead)
+def get_user(
+    user_id: int,
+    session: Session = Depends(get_session)
+):
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+
+@router.post("/", response_model=UserRead)
+def create_user(
+    payload: UserCreate,
+    session: Session = Depends(get_session)
+                        ):
+    user = User.model_validate(payload)
+    session.add(user)
+
+    try:
+        session.commit()
+
+    except IntegrityError:
+        session.rollback()
+        raise HTTPException(
+            status_code=409,
+            detail="Invalid reference or duplicate User data"
+        )
+    session.refresh(user)
+    return user
