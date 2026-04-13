953f0297f2ba2bcbdd44a009bdca490820822caf
Puneeth A L <puneeth.fcv@gmail.com>
2026-04-13 11:09:32 +0530

    chore(app): update api router

--- app/api/api_router.py
+++ app/api/api_router.py
@@ -11,7 +11,6 @@ from app.api.router.user import router as user_router
 from app.api.router.bill import router as bill_router
 from app.api.router.auth import router as auth_router
 from app.api.router.reports.reports_1_2 import router as reports_1_2_router
-# from app.api.router.reports.report_3 import router as report_3_router
 from app.api.router.reports.report_4_collections import router as report_4_router
 from app.api.router.reports.report_5_agent_bills import router as report_5_router
 from app.api.router.agent_router import router as agent_router
@@ -31,7 +30,6 @@ api_router.include_router(user_router)
 api_router.include_router(bill_router)
 api_router.include_router(auth_router)
 api_router.include_router(reports_1_2_router)
-# api_router.include_router(report_3_router)
 api_router.include_router(report_4_router)
 api_router.include_router(report_5_router)
 api_router.include_router(agent_router)
